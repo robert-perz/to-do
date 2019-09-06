2c0625a0121d96b813f3f90ddb7c1fc8c13ff0e8
robert-perz <robert.perz@outlook.com>
2019-09-06 16:25:49 +0200

    Updates

---
+++ precache-manifest.984c2b5ccb613d3465a6a95acef8bc16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778a9b681839c67325186ed5df2473cd",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "8a03c00ea60dd38ba646",
+    "url": "/to-do/static/css/main.b6118809.chunk.css"
+  },
+  {
+    "revision": "41bcd19ccecd5ae29232",
+    "url": "/to-do/static/js/2.5f6b880c.chunk.js"
+  },
+  {
+    "revision": "8a03c00ea60dd38ba646",
+    "url": "/to-do/static/js/main.504cfe66.chunk.js"
+  },
+  {
+    "revision": "b000c68b059018af0ede",
+    "url": "/to-do/static/js/runtime~main.2bc456c2.js"
+  },
+  {
+    "revision": "f46858722e2b6c2d3d6efaee100c13a5",
+    "url": "/to-do/static/media/nyc.f4685872.jpg"
+  }
+]);
